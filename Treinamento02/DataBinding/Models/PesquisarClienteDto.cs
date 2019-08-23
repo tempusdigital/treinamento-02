b2dd88f7e82ce1caafcd73fa906e781c86260f8e
Bruno Albano de Souza <brunoalbano@tempus.digital>
2019-08-22 22:14:23 -0300

    Exemplos de DataBinding

---
+++ Treinamento02/DataBinding/Models/PesquisarClienteDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataBinding.Models
+{
+    public class PesquisarClienteDto
+    {
+        public string Cpf { get; set; }
+
+        public DateTime? Data { get; set; }
+        
+        public decimal? Score { get; set; }
+    }
+}
